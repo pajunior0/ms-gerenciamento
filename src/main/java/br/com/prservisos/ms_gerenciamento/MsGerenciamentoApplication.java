a4cf1f1f78c67ba3691ab2e53fec818e53bcc4eb
Paulo Junior <pajunior0@gmail.com>
2024-08-15 00:00:00 -0300

    primeiro commit

---
+++ src/main/java/br/com/prservisos/ms_gerenciamento/MsGerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.prservisos.ms_gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGerenciamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/prservisos/ms_gerenciamento/MsGerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prservisos.ms_gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
